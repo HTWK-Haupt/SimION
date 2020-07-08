ce32da57e95928075a076f22fc4bc89937c6c72f
Peter Haupt <peter.haupt@htwk-leipzig.de>
2020-07-08 16:52:45 +0200

    Missing files added

---
+++ Firmware_ESP32/LM_RTL_V0_4c2_types.h
@@ -0,0 +1,22 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ * File: LM_RTL_V0_4c2_types.h
+ *
+ * MATLAB Coder version            : 5.0
+ * C/C++ source code generated on  : 06-Jul-2020 15:55:24
+ */
+
+#ifndef LM_RTL_V0_4C2_TYPES_H
+#define LM_RTL_V0_4C2_TYPES_H
+
+/* Include Files */
+#include "rtwtypes.h"
+#endif
+
+/*
+ * File trailer for LM_RTL_V0_4c2_types.h
+ *
+ * [EOF]
+ */
